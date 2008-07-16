3ff183b23ad1a2aa4240aeb4bd69efa5208b9dd4
Pius Uzamere <pius+git@alum.mit.edu>
2008-07-16 00:44:03 -0400

    modularized the SPARQL grammars

--- lib/sparql/execute_sparql.rb
+++ lib/sparql/execute_sparql.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'treetop'
 
+Treetop.load "lib/sparql/primitives"
+Treetop.load "lib/sparql/variables"
+Treetop.load "lib/sparql/logical_expressions"
+Treetop.load "lib/sparql/graph"
+Treetop.load "lib/sparql/series"
 Treetop.load "lib/sparql/sparql"
-#Treetop.load "lib/sparql/iri"
