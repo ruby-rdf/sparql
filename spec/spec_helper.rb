afbb205d065dc58430bf668ffd55a670d0b238d8
Pius Uzamere <pius+git@alum.mit.edu>
2008-07-15 22:59:20 -0400

    added support for more complex SELECT statements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,13 @@ require 'rubygems'
 require 'pathname'
 require 'treetop'
 
+#'WHERE'? space '{' space series_of_triple_patterns space '}'
+
+# rule triples_same_subject
+#   var_or_term space property_list_not_empty / triples_node space property_list
+# end
+
+
 class Pathname
   def /(path)
     (self + path).expand_path
--- spec/unit/check_parsing_spec.rb
+++ spec/unit/check_parsing_spec.rb
@@ -1,7 +1,7 @@
 require 'pathname'
 
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
-
+		#'SELECT ?foo WHERE { ?x foaf:knows ?y . }'
 describe "SparqlParser", '#parse' do
   
   before(:all) do
@@ -20,10 +20,27 @@ describe "SparqlParser", '#parse' do
     basic_valid_select_query_with_prologue = 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?foo WHERE { ?x foaf:knows ?y . }'
     parser = SparqlParser.new
     parser.parse(basic_valid_select_query_with_prologue).well_formed?.should == true
-    # parser.parse(basic_valid_select_query_with_prologue).prologue.text_value.should == ""
-    #  parser.parse(basic_valid_select_query_with_prologue).query_part.text_value.should == "SELECT ?foo WHERE { ?x foaf:knows ?y . }"
-    #  parser.parse('SELECT ?foo ?bar WHERE { ?x foaf:knows ?y . ?z foaf:name ?y . }').should_not == nil
+    parser.parse(basic_valid_select_query_with_prologue).prologue.text_value.should == "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
+    parser.parse(basic_valid_select_query_with_prologue).query_part.text_value.should == "SELECT ?foo WHERE { ?x foaf:knows ?y . }"
+  end
+   
+   it "should recognize a basic SELECT query with one variable, two triple patterns, and a PREFIX declaration" do
+     query = 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?foo WHERE { ?x foaf:knows ?y . ?z foaf:knows ?x .}'
+     parser = SparqlParser.new
+     parser.parse(query).well_formed?.should == true
+     parser.parse(query).prologue.text_value.should == "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
+     parser.parse(query).query_part.text_value.should == "SELECT ?foo WHERE { ?x foaf:knows ?y . ?z foaf:knows ?x .}"
    end
+   
+   it "should recognize a SELECT query with a LANGTAG" do
+     query = 'SELECT ?v WHERE { ?v ?p "cat"@en }'
+     parser = SparqlParser.new
+     parser.parse(query).well_formed?.should == true
+     parser.parse(query).prologue.text_value.should == ""
+     parser.parse(query).query_part.text_value.should == 'SELECT ?v WHERE { ?v ?p "cat"@en }'
+   end
+   
+
 
   
   it "should recognize a CONSTRUCT query"
