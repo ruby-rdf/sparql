0c598e557cb053268ad5a6dcadf01465dacc4f00
Pius Uzamere <pius+git@alum.mit.edu>
2008-07-15 20:27:47 -0400

    added another test case plus an awful hack for getting around my trouble with parsing IRIs

--- lib/sparql/execute_sparql.rb
+++ lib/sparql/execute_sparql.rb
@@ -2,3 +2,4 @@ require 'rubygems'
 require 'treetop'
 
 Treetop.load "lib/sparql/sparql"
+#Treetop.load "lib/sparql/iri"
--- spec/unit/check_parsing_spec.rb
+++ spec/unit/check_parsing_spec.rb
@@ -7,15 +7,24 @@ describe "SparqlParser", '#parse' do
   before(:all) do
   end
   
-  it "should recognize a SELECT query" do
+  it "should recognize a basic SELECT query with one variable and one triple pattern" do
+    basic_valid_select_query = 'SELECT ?foo WHERE { ?x foaf:knows ?y . }'
     parser = SparqlParser.new
-    #parser.parse('?foo').should == nil
-    #parser.parse('?x foaf:knows ?y .').should == nil
-    #parser.parse('WHERE { ?x foaf:knows ?y . }').should_not == nil
-    parser.parse('SELECT ?foo WHERE { ?x foaf:knows ?y . }').should_not == nil
-    parser.parse('SELECT ?foo ?bar WHERE { ?x foaf:knows ?y . }').should_not == nil
+    parser.parse(basic_valid_select_query).well_formed?.should == true
+    parser.parse(basic_valid_select_query).prologue.text_value.should == ""
+    parser.parse(basic_valid_select_query).query_part.text_value.should == "SELECT ?foo WHERE { ?x foaf:knows ?y . }"
     parser.parse('SELECT ?foo ?bar WHERE { ?x foaf:knows ?y . ?z foaf:name ?y . }').should_not == nil
   end
+  
+  it "should recognize a basic SELECT query with one variable, one triple pattern, and a PREFIX declaration" do
+    basic_valid_select_query_with_prologue = 'PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?foo WHERE { ?x foaf:knows ?y . }'
+    parser = SparqlParser.new
+    parser.parse(basic_valid_select_query_with_prologue).well_formed?.should == true
+    # parser.parse(basic_valid_select_query_with_prologue).prologue.text_value.should == ""
+    #  parser.parse(basic_valid_select_query_with_prologue).query_part.text_value.should == "SELECT ?foo WHERE { ?x foaf:knows ?y . }"
+    #  parser.parse('SELECT ?foo ?bar WHERE { ?x foaf:knows ?y . ?z foaf:name ?y . }').should_not == nil
+   end
+
   
   it "should recognize a CONSTRUCT query"
   it "should recognize an ASK query"
