c07b844264b2912c9f826dcefc8f48823315e411
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-09-28 13:44:57 -0700

    readme_spec.

---
+++ spec/readme_spec.rb
@@ -0,0 +1,45 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'linkeddata'
+require 'strscan'
+
+describe "README" do
+  describe SPARQL do
+  end
+  
+  def self.read_examples
+    examples = []
+    readme = File.join(File.expand_path(File.dirname(__FILE__)), "..", "README.markdown")
+    scanner = StringScanner.new(File.read(readme))
+    scanner.skip_until(/^## Examples$/)
+    until scanner.eos?
+      break if scanner.matched =~ /## Documentation/
+      title = scanner.matched.match(/^### (.*)$/) && $1
+      code = scanner.scan_until(/^##.*$/)
+      code = code.match(/^(.*)^##.*$/m) && $1
+      case title
+      when "Command line processing"
+        code.split("\n").reject {|c| c =~ /^\s*(?:#.*)?$/}.each do |command|
+          examples << {:title => command, :sh => command}
+        end
+      else
+        examples << {:title => title, :eval_true => code}
+      end
+    end
+    examples
+  end
+
+  read_examples.each do |example|
+    it "Example #{example[:title] || 'require'}" do
+      if example[:eval_true]
+        cmd = example[:eval_true].
+          gsub('etc', File.join(File.dirname(__FILE__), '..', 'etc'))
+        eval(cmd)
+      else
+        cmd = example[:sh].
+          sub('sparql', File.join(File.dirname(__FILE__), '..', 'bin', 'sparql')).
+          sub('etc', File.join(File.dirname(__FILE__), '..', 'etc'))
+        IO.popen(cmd) {|io| io.read}.should be_true
+      end
+    end
+  end
+end
