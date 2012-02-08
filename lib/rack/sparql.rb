2e15bd0ac67492b1160e2c81f8062f8180289b4f
Gregg Kellogg <gregg@kellogg-assoc.com>
2012-02-08 13:16:26 -0800

    Register format symbols as MIME file extensions, as formats are often known both by symbol and file extension.

--- lib/rack/sparql.rb
+++ lib/rack/sparql.rb
@@ -9,11 +9,18 @@ module Rack
     ##
     # Registers all known RDF formats with Rack's MIME types registry.
     #
+    # Registers both known file extensions and format symbols.
+    #
     # @param  [Hash{Symbol => Object}] options
     # @option options [Boolean]        :overwrite (false)
     # @return [void]
     def self.register_mime_types!(options = {})
       if defined?(Rack::Mime::MIME_TYPES)
+        RDF::Format.each do |format|
+          if !Rack::Mime::MIME_TYPES.has_key?(file_ext = ".#{format.to_sym}") || options[:overwrite]
+            Rack::Mime::MIME_TYPES.merge!(file_ext => format.content_type.first)
+          end
+        end
         RDF::Format.file_extensions.each do |file_ext, formats|
           if !Rack::Mime::MIME_TYPES.has_key?(file_ext = ".#{file_ext}") || options[:overwrite]
             Rack::Mime::MIME_TYPES.merge!(file_ext => formats.first.content_type.first)
