422cadfd012620e69a8ca9a747fbae5a9fb57cf1
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-01-29 16:46:35 -0800

    Progress on updating the (dataset) operator to load default graphs into the repository using their graph names, and specify the default graphs as using a default match, rathern than a named match, so that the specific "named" graphs will match as either/or default (meaning context == false) or named (meaning context == IRI or variable).

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source "http://rubygems.org"
 
 gemspec :name => ""
 
-gem "rdf",            :git => "git://github.com/ruby-rdf/rdf.git"
+#gem "rdf",            :git => "git://github.com/ruby-rdf/rdf.git"
+gem 'rdf', :path => "../rdf"
 gem "rdf-spec",       :git => "git://github.com/ruby-rdf/rdf-spec.git"
 gem "rdf-xsd",        :git => "git://github.com/ruby-rdf/rdf-xsd.git"
 
--- lib/sparql/algebra/operator/dataset.rb
+++ lib/sparql/algebra/operator/dataset.rb
@@ -4,25 +4,48 @@ rescue LoadError => e
   require 'rdf/ntriples'
 end
 
+# FIXME: This version uses named graphs for default graphs, which violates the condition in RDF::Repository#query_pattern, where it specifically does not match variables against the default graph. To work properly, RDF.rb will need to allow some way to specify a set of graphs as being default, and affect the matching within #query_pattern so that variables don't match against this.
+# Note that a graph may be both default and named, so the context of the query is significant.
 module SPARQL; module Algebra
   class Operator
     ##
     # The SPARQL GraphPattern `dataset` operator.
     #
-    # Instintiated with two operands, the first being an array of data source URIs,
+    # Instantiated with two operands, the first being an array of data source URIs,
     # either bare, indicating a default dataset, or expressed as an array `\[:named, \<uri\>\]`,
     # indicating that it represents a named data source.
+    #
+    # This operator loads the document referenced by the URI into the dataset
+    # using `uri` as the graph name, unless it already exists within the dataset.
+    #
+    # The contained BGP queries are then performed against the specified
+    # default and named graphs. Rather than using the actual default
+    # graph of the dataset, queries against the default dataset are
+    # run against named graphs matching a non-distinctive variable
+    # and the results are filtered against those URIs included in
+    # the default dataset.
     #
     # @example
     #
     #     (prefix ((: <http://example/>))
     #       (dataset (<data-g1.ttl> (named <data-g2.ttl>))
-    #         (bgp (triple ?s ?p ?o))))
+    #         (union
+    #           (bgp (triple ?s ?p ?o))
+    #           (graph ?g (bgp (triple ?s ?p ?o))))))
+    #
+    # is effectively re-written to the following:
+    #
+    #     (prefix ((: <http://example/>))
+    #       (dataset (<data-g1.ttl> (named <data-g2.ttl>))
+    #         (filter (= ??g <data-g1.ttl>)
+    #           (union
+    #             (graph ??g (bgp (triple ?s ?p ?o)))
+    #             (graph ?g (bgp (triple ?s ?p ?o)))))))
     #
     # @see http://www.w3.org/TR/rdf-sparql-query/#specifyingDataset
     class Dataset < Binary
       include Query
-      
+
       NAME = [:dataset]
       # Selected accept headers, from those available
       ACCEPTS = (%w(
@@ -52,6 +75,7 @@ module SPARQL; module Algebra
       # @see    http://www.w3.org/TR/rdf-sparql-query/#sparqlAlgebra
       def execute(queryable, options = {})
         debug(options) {"Dataset"}
+        default_graphs = []
         operand(0).each do |ds|
           load_opts = {
             :headers => {"Accept" => ACCEPTS}
@@ -69,18 +93,41 @@ module SPARQL; module Algebra
             end
             uri = self.base_uri ? self.base_uri.join(ds.last) : ds.last
             uri.lexical = ds.last
-            load_opts[:context] = uri
-            debug(options) {"=> read named data source #{uri}"}
+            debug(options) {"=> named data source #{uri}"}
           else
             debug(options) {"=> array: join #{self.base_uri.inspect} to #{ds.inspect}"}
             uri = self.base_uri ? self.base_uri.join(ds) : ds
-            debug(options) {"=> read default data source #{uri}"}
+            debug(options) {"=> default data source #{uri}"}
+            default_graphs << uri
           end
-          load_opts[:base_uri] = uri
-          queryable.load(uri.to_s, load_opts)
+          load_opts[:context] = load_opts[:base_uri] = uri
+          unless queryable.has_context?(uri)
+            debug(options) {"=> load #{uri}"}
+            queryable.load(uri.to_s, load_opts)
+          end
         end
 
-        @solutions = operands.last.execute(queryable, options.merge(:depth => options[:depth].to_i + 1))
+        # Query binding a non-distinguishded variable to context
+        default_var = RDF::Query::Variable.new
+        default_var.distinguished = false
+
+        @solutions = operands.last.execute(queryable, options.merge(
+          :context => default_var,
+          :depth => options[:depth].to_i + 1)
+        ).filter do |soln|
+          # Reject solutions with bindings to default_var where the value
+          # is not a specified default graph
+          debug(options) {"=> filter: #{soln.inspect}"}
+          if soln.unbound?(default_var)
+            true
+          elsif default_graphs.include?(soln[default_var])
+            # Remove the variable from the solution and match
+            # FIXME: this should either go in RDF::Query::Solution,
+            # or there should be a immutable way of performing this
+            # as an operation on RDF::Query::Solutions
+            soln.bindings.delete(default_var.to_sym)
+          end
+        end
       end
       
       ##
