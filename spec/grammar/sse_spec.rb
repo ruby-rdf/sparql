9365d55658a53c8f7725023b247499b27515b7ce
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-03-10 22:56:51 -0700

    Run all syntax and evaluation tests to check for expected output. Also run negative syntax tests.

--- spec/grammar/sse_spec.rb
+++ spec/grammar/sse_spec.rb
@@ -2,51 +2,79 @@ $:.unshift ".."
 require 'spec_helper'
 require 'dawg_helper'
 
-describe SPARQL::Grammar::Parser do
-  describe "w3c dawg SPARQL 1.0 tests to SSE" do
-    SPARQL::Spec.sparql1_0_tests.group_by(&:manifest).each do |man, tests|
-      describe man.to_s.split("/")[-2] do
-        tests.each do |t|
-          case t.type
-          when MF.QueryEvaluationTest
-            it "parses #{t.name} to correct SSE" do
-              case t.name
-              when 'Basic - Term 6', 'Basic - Term 7'
-                pending "Decimal format changed in SPARQL 1.1"
-              end
-              parser_opts = {:base_uri => t.action.query_file}
-              parser_opts[:debug] = true if ENV['PARSER_DEBUG']
-              query = SPARQL::Grammar.parse(t.action.query_string, parser_opts)
-              sse = SPARQL::Algebra.parse(t.action.sse_string, parser_opts)
-              query.should == sse
-            end
-
-            it "parses #{t.name} to lexically equivalent SSE" do
-              case t.name
-              when 'Basic - Term 7'
-                pending "Decimal format changed in SPARQL 1.1"
-              end
-              query = SPARQL::Grammar.parse(t.action.query_string)
-              normalized_query = query.to_sxp.
-                gsub(/\s+/m, " ").
-                gsub(/\(\s+\(/, '((').
-                gsub(/\)\s+\)/, '))').
-                strip
-              normalized_result = t.action.sse_string.
-                gsub(/\s+/m, " ").
-                gsub(/\(\s+\(/, '((').
-                gsub(/\)\s+\)/, '))').
-                strip
-              normalized_query.should == normalized_result rescue pending("Lexical equivalence")
-            end
-          else
-            it "??? #{t.name}" do
-              puts t.inspect
-              fail "Unknown test type #{t.type}"
-            end
+shared_examples "SSE" do |man, tests|
+  describe man.to_s.split("/")[-2] do
+    tests.each do |t|
+      case t.type
+      when MF.QueryEvaluationTest, MF.PositiveSyntaxTest
+        it "parses #{t.name} to correct SSE" do
+          case t.name
+          when 'Basic - Term 6', 'Basic - Term 7', 'syntax-lit-08.rq'
+            pending "Decimal format changed in SPARQL 1.1"
+          when 'syntax-esc-04.rq', 'syntax-esc-05.rq'
+            pending "Fixing PNAME_LN not matching :\\u0070"
           end
+          parser_opts = {:base_uri => t.action.query_file}
+          parser_opts[:debug] = true if ENV['PARSER_DEBUG']
+          query = SPARQL::Grammar.parse(t.action.query_string, parser_opts)
+          sse = SPARQL::Algebra.parse(t.action.sse_string, parser_opts)
+          query.should == sse
+        end
+
+        it "parses #{t.name} to lexically equivalent SSE" do
+          case t.name
+          when 'Basic - Term 6', 'Basic - Term 7', 'syntax-lit-08.rq'
+            pending "Decimal format changed in SPARQL 1.1"
+          when 'syntax-esc-04.rq', 'syntax-esc-05.rq'
+            pending "Fixing PNAME_LN not matching :\\u0070"
+          end
+          query = begin
+            SPARQL::Grammar.parse(t.action.query_string, :debug => ENV['PARSER_DEBUG'])
+          rescue Exception => e
+            "Error: #{e.message}"
+          end
+          normalized_query = query.to_sxp.
+            gsub(/\s+/m, " ").
+            gsub(/\(\s+\(/, '((').
+            gsub(/\)\s+\)/, '))').
+            strip
+          normalized_result = t.action.sse_string.
+            gsub(/\s+/m, " ").
+            gsub(/\(\s+\(/, '((').
+            gsub(/\)\s+\)/, '))').
+            strip
+          normalized_query.should == normalized_result
+        end
+      when MF.NegativeSyntaxTest
+        it "detects syntax error for #{t.name}" do
+          begin
+            lambda {SPARQL::Grammar.parse(t.action.query_string, :validate => true)}.should raise_error
+          rescue
+            pending "Detecting syntax errors better"
+          end
+        end
+      else
+        it "??? #{t.name}" do
+          puts t.inspect
+          fail "Unknown test type #{t.type}"
         end
       end
     end
   end
-end unless ENV['CI']
+end
+
+unless ENV['CI']
+  describe SPARQL::Grammar::Parser do
+    describe "w3c dawg SPARQL 1.0 syntax tests" do
+      SPARQL::Spec.sparql1_0_syntax_tests.group_by(&:manifest).each do |man, tests|
+        it_behaves_like "SSE", man, tests
+      end
+    end
+
+    describe "w3c dawg SPARQL 1.0 tests" do
+      SPARQL::Spec.sparql1_0_tests.group_by(&:manifest).each do |man, tests|
+        it_behaves_like "SSE", man, tests
+      end
+    end
+  end
+end
