d1c9584677dd7594343a35d02fa2790ca2499122
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-03-12 13:07:00 -0700

    Update DAWG infrastructure for differences in base locations for 1.0 and 1.1 tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,10 +32,12 @@ URI_CACHE = File.expand_path(File.join(File.dirname(__FILE__), "uri-cache"))
 Dir.mkdir(URI_CACHE) unless File.directory?(URI_CACHE)
 OpenURI::Cache.class_eval { @cache_path = URI_CACHE }
 
-DAWG = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#')
-MF   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#')
-QT   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-query#')
-RS   = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/result-set#')
+DAWGT = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-dawg#')
+ENT   = RDF::Vocabulary.new('http://www.w3.org/ns/entailment/RDF')
+MF    = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#')
+QT    = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/test-query#')
+RS    = RDF::Vocabulary.new('http://www.w3.org/2001/sw/DataAccess/tests/result-set#')
+UT    = RDF::Vocabulary.new('http://www.w3.org/2009/sparql/tests/test-update#')
 
 def repr(term)
   case term
