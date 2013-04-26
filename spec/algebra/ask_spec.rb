94a12cdc4af3f05a76d1d09e90166fc92587d4e5
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-04-25 19:43:51 -0700

    Create SSE construct_spec with bound/unbound query tests. Separate ask and describe specs out from query_spec. This addresses issue #8, but requies un-released changes to the rdf gem.

---
+++ spec/algebra/ask_spec.rb
@@ -0,0 +1,46 @@
+$:.unshift ".."
+require 'spec_helper'
+require 'algebra/algebra_helper'
+require 'sparql/client'
+
+include SPARQL::Algebra
+
+::RSpec::Matchers.define :have_result_set do |expected|
+  match do |result|
+    result.map(&:to_hash).to_set.should == expected.to_set
+  end
+end
+
+describe SPARQL::Algebra::Query do
+  EX = RDF::EX = RDF::Vocabulary.new('http://example.org/') unless const_defined?(:EX)
+
+  context "ask" do
+    it "passes data-r2/as/ask-1" do
+      sparql_query(
+        :form => :ask,
+        :graphs => {
+          :default => {
+            :data => %q{
+              @prefix :   <http://example/> .
+              @prefix xsd:        <http://www.w3.org/2001/XMLSchema#> .
+
+              :x :p "1"^^xsd:integer .
+              :x :p "2"^^xsd:integer .
+              :x :p "3"^^xsd:integer .
+
+              :y :p :a .
+              :a :q :r .
+            },
+            :format => :ttl
+          }
+        },
+        :query => %q{
+          (prefix ((: <http://example/>))
+          (ask
+            (bgp (triple :x :p 1))))
+        },
+        :sse => true
+      ).should be_true
+    end
+  end
+end
---
+++ spec/algebra/construct_spec.rb
@@ -0,0 +1,100 @@
+$:.unshift ".."
+require 'spec_helper'
+require 'algebra/algebra_helper'
+require 'sparql/client'
+
+include SPARQL::Algebra
+
+::RSpec::Matchers.define :have_result_set do |expected|
+  match do |result|
+    result.map(&:to_hash).to_set.should == expected.to_set
+  end
+end
+
+describe SPARQL::Algebra::Query do
+  EX = RDF::EX = RDF::Vocabulary.new('http://example.org/') unless const_defined?(:EX)
+
+  context "ask" do
+    {
+      "query-construct-optional" => [
+        %q{
+          @prefix : <http://example/> .
+          @prefix xsd:        <http://www.w3.org/2001/XMLSchema#> .
+
+          :x :p :a .
+          :x :p :b .
+          :x :p :c .
+          :x :p "1"^^xsd:integer .
+
+          :a :q "2"^^xsd:integer .
+          :a :r "2"^^xsd:integer .
+
+          :b :q "2"^^xsd:integer .
+        },
+        %q{
+          @prefix :        <http://example/> .
+          @prefix xsd:        <http://www.w3.org/2001/XMLSchema#> .
+
+          :x    :p2           "2"^^xsd:integer .
+        },
+        %q{
+          (prefix ((: <http://example/>))
+            (construct ((triple ?x :p2 ?v))
+              (leftjoin
+                (bgp (triple ?x :p ?o))
+                (bgp (triple ?o :q ?v)))))
+        },
+      ],
+      "bound node" => [
+        %q(
+          @prefix ex: <http://example.com/> .
+ 
+          [] a ex:bound .
+          [] a ex:somethingElse .
+        ),
+        %q(
+          @prefix ex: <http://example.com/> .
+          [] ex:label "Should appear once" .
+        ),
+        %q(
+          (prefix
+           ((ex: <http://example.com/>))
+           (construct ((triple _:b0 ex:label "Should appear once"))
+            (bgp (triple ??0 a ex:bound))))
+        )
+      ],
+      "unbound node" => [
+        %q(
+          @prefix ex: <http://example.com/> .
+ 
+          [] a ex:bound .
+          [] a ex:somethingElse .
+        ),
+        %q(
+          @prefix ex: <http://example.com/> .
+          [] ex:label "Should appear once" .
+        ),
+        %q(
+          (prefix
+           ((ex: <http://example.com/>))
+           (construct
+            ((triple _:b0 ex:label "Should appear once"))
+            (filter (! (bound ?s))
+              (leftjoin
+                (bgp)
+                (bgp (triple ?s ?p ex:not-bound)))))
+          )
+        )
+      ],
+    }.each do |example, (source, result, query)|
+      it "constructs #{example}" do
+        graph_r = RDF::Graph.new << RDF::Turtle::Reader.new(result)
+
+        sparql_query(
+          :form => :describe, :sse => true,
+          :graphs => {:default => {:data => source, :format => :ttl}},
+          :query => query).should == graph_r
+      end
+    end
+  end
+end
