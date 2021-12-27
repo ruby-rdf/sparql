4629d8f5fc0656a99d62c132e360ee1a95941d32
Gregg Kellogg <gregg@greggkellogg.net>
2021-12-27 13:21:55 -0800

    subtract examples.

--- lib/sparql/algebra/operator/subtract.rb
+++ lib/sparql/algebra/operator/subtract.rb
@@ -2,8 +2,25 @@ module SPARQL; module Algebra
   class Operator
     ##
     # The SPARQL numeric `subtract` operator.
-    #   (- ?x ?y)
-    #   (subtract ?x ?y)
+    #
+    # [116] AdditiveExpression ::=	MultiplicativeExpression ( '-' MultiplicativeExpression )?
+    #
+    # @example SPARQL Grammar
+    #   PREFIX : <http://example.org/>
+    #   SELECT ?s WHERE {
+    #     ?s :p ?o .
+    #     ?s2 :p ?o2 .
+    #     FILTER(?o - ?o2 = 3) .
+    #   }
+    #
+    # @example SSE
+    #   (prefix
+    #    ((: <http://example.org/>))
+    #    (project (?s)
+    #     (filter (= (- ?o ?o2) 3)
+    #      (bgp
+    #       (triple ?s :p ?o)
+    #       (triple ?s2 :p ?o2)))))
     #
     # @see https://www.w3.org/TR/xpath-functions/#func-numeric-subtract
     class Subtract < Operator::Binary
