94bd7a1ba3f4c6ce0035ff4e12a7f7f2af4dba9b
Gregg Kellogg <gregg@greggkellogg.net>
2022-01-09 17:19:56 -0800

    Mark remaining SPARQL 1.1 to_sparql tests are pending.

--- spec/suite_spec.rb
+++ spec/suite_spec.rb
@@ -154,6 +154,21 @@ shared_examples "to_sparql" do |id, label, comment, tests|
               'agg-sample-01.rq', 'sq03.rq', 'sq08.rq', 'sq09.rq', 'sq11.rq', 'sq12.rq',
               'sq13.rq', 'sq14.rq', 'syntax-SELECTscope1.rq', 'syntax-SELECTscope3.rq'
             pending("TODO SubSelect")
+          when 'agg-err-01.rq'
+            pending "TODO key not found"
+          when 'pp06.rq', 'path-ng-01.rq', 'path-ng-02.rq'
+            pending "TODO graph name on property path"
+          when 'pp09.rq', 'pp10.rq', 'path-p2.rq', 'path-p4.rq'
+            pending "TODO property path grouping"
+          when 'syntax-bindings-02a.rq', 'syntax-bindings-03a.rq', 'syntax-bindings-05a.rq'
+            pending "TODO top-level values"
+          when 'syn-pname-05.rq', 'syn-pname-06.rq', 'syn-pname-07.rq', 'syn-codepoint-escape-01.rq',
+               '1val1STRING_LITERAL1_with_UTF8_boundaries.rq', '1val1STRING_LITERAL1_with_UTF8_boundaries_escaped.rq'
+            pending "TODO escaping"
+          when 'syn-pp-in-collection.rq'
+            pending "TODO runtime error and list representation"
+          when 'strafter02.rq '
+            pending "TODO odd project multple bindings"
           end
           t.logger = RDF::Spec.logger
           t.logger.debug "Source:\n#{t.action.query_string}"
@@ -172,16 +187,30 @@ shared_examples "to_sparql" do |id, label, comment, tests|
               'insert-where-same-bnode.ru', 'insert-where-same-bnode2.ru',
               'delete-insert-04.ru'
             pending("SubSelect")
+          when 'delete-insert-04b.ru', 'delete-insert-05b.ru', 'delete-insert-05b.ru'
+            pending "TODO sub-joins"
+          when 'syntax-update-38.ru'
+            pending "empty query"
+          when 'large-request-01.ru'
+            skip "large request"
           end
+          pending("Whitespace in string tokens") if %w(
+            syntax-update-26.ru syntax-update-27.ru syntax-update-28.ru
+            syntax-update-36.ru
+          ).include?(t.entry)
           t.logger = RDF::Spec.logger
           t.logger.debug "Source:\n#{t.action.query_string}\n"
           sse = SPARQL.parse(t.action.query_string,
                              base_uri: t.base_uri,
                              update: true)
           sparql = sse.to_sparql(base_uri: t.base_uri)
-          expect(sparql).to generate(sse, resolve_iris: false, production: :UpdateUnit, logger: t.logger)
+          expect(sparql).to generate(sse,
+                                     base_uri: t.base_uri,
+                                     resolve_iris: false,
+                                     production: :UpdateUnit,
+                                     logger: t.logger)
         end
-      when 'mf:NegativeSyntaxTest', 'mf:NegativeSyntaxTest11'
+      when 'mf:NegativeSyntaxTest', 'mf:NegativeSyntaxTest11', 'mf:NegativeUpdateSyntaxTest11'
         # Do nothing.
       else
         it "??? #{t.entry} - #{t.name}" do
@@ -217,7 +246,7 @@ describe SPARQL do
     SPARQL::Spec.sparql1_1_tests.each do |path|
       SPARQL::Spec::Manifest.open(path) do |man|
         it_behaves_like "SUITE", man.attributes['id'], man.label, man.comment, man.entries
-        #it_behaves_like "to_sparql", man.attributes['id'], man.label, man.comment, man.entries
+        it_behaves_like "to_sparql", man.attributes['id'], man.label, man.comment, man.entries
       end
     end
   end
